86fc86ad0e458d332483c2e0e17aa7c7187c1491
AimanTalib <shaikhaiman1012@gmail.com>
2024-01-25 14:48:40 +0500

    auth done

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        if(Auth::id())
+        {
+            $usertype =Auth()->user()->usertype;
+            
+            if($usertype=='user')
+            {
+                return view('user_panel.user_dashboard');
+            }
+            else if($usertype=='admin')
+            {
+                return view('admin_panel.admin_dasboard');
+            }
+        }
+        else
+        {
+            return redirect()->back();
+        }
+    }
+    public function adminpage()
+    {
+        return view('admin_panel.admin_page');
+    }
+}
